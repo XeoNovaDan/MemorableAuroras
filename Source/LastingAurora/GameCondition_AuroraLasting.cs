8730542d262a2a59e4f1e58dfe7549d0fe3c5b88
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-07-10 21:52:46 +0100

    Initial commit

---
+++ Source/LastingAurora/GameCondition_AuroraLasting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace LastingAurora
+{
+    public class GameCondition_AuroraLasting : GameCondition_Aurora
+    {
+
+        public override void GameConditionTick()
+        {
+            base.GameConditionTick();
+            foreach (Map map in AffectedMaps)
+            {
+                for (int i = 0; i < map.mapPawns.AllPawnsSpawnedCount; i++)
+                {
+                    Pawn pawn = map.mapPawns.AllPawnsSpawned[i];
+                    if (pawn.needs.mood != null && !pawn.PositionHeld.Roofed(map) && pawn.health.capacities.CapableOf(PawnCapacityDefOf.Sight))
+                        pawn.needs.mood.thoughts.memories.TryGainMemory((Thought_Memory)ThoughtMaker.MakeThought(LA_ThoughtDefOf.ObservedAurora));
+                }
+            }
+        }
+
+    }
+}
---
+++ Source/LastingAurora/LA_ThoughtDefOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace LastingAurora
+{
+
+    [DefOf]
+    public static class LA_ThoughtDefOf
+    {
+
+        public static ThoughtDef ObservedAurora;
+
+    }
+
+}
